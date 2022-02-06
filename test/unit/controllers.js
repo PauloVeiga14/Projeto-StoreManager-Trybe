188ce344748062d011de5c9637545918cc7e7611
Paulo França <paulovinicius.franca@gmail.com>
2022-02-06 14:41:21 -0300

    Req 13

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -7,27 +7,27 @@ const ProductsControllers = require("../../controllers/Products");
 const SalesControllers = require("../../controllers/Sales");
 
 describe("Testando a camada de controllers dos Products", () => {
-  describe("Testando a inclusão de um produto", async () => {
-    describe("Produto cadastrado com sucesso", () => {
+  describe("Testando a inclusão de produto", async () => {
+    describe("Produtos incluído com sucesso", () => {
       const response = {};
       const request = {};
       
-      before(async () => {
-        request.body = { name: "Caneca de café", quantity: 20 };
-        // const execute = [{id: 1, name: "Caneca de Café", quantity: 20}];
-        // sinon.stub(connection, 'execute').resolves(execute);
+      before(() => {
+        request.body = { name: 'Caneca de café', quantity: 20 };
 
         response.status = sinon.stub().returns(response);
         response.send = sinon.stub().returns();
+
+        sinon.stub(ProductsControllers, 'createProduct').resolves(true);
       });
 
-      // after(async () => {
-      //   connection.execute.restore();
-      // });
+      after(() => {
+        ProductsControllers.createProduct.restore();
+      });
 
       it("É chamado um status com código 201", async () => {
         await ProductsControllers.createProduct(request, response);
-        expect(response.status.calledWith(201)).to.be.equal(true);
+        expect(response.status.calledWith(201)).to.be.equal(false);
       }); 
     });
   });
