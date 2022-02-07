e2a1686310f0629ba112d2e5470a12dd4b86367a
Paulo França <paulovinicius.franca@gmail.com>
2022-02-07 20:09:56 -0300

    Fim de projeto?

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -7,7 +7,7 @@ const ProductsControllers = require("../../controllers/Products");
 const ProductsModels = require("../../models/Products");
 const SalesControllers = require("../../controllers/Sales");
 const SalesServices = require("../../services/Sales");
-const req = require("express/lib/request");
+const SalesModels = require("../../models/Sales")
 
 describe("Testando a camada de controllers dos Products", () => {
   describe("Testando a listagem de produtos", async () => {
@@ -91,8 +91,36 @@ describe("Testando a camada de controllers dos Sales", () => {
         await SalesControllers.getSaleById(request, response);
         expect(response.status.calledWith(200)).to.be.equal(true);
       });
+    });
+  });
 
+  describe("Testando o upgrade de uma venda", async () => {
+    describe("Upgrade realizado com sucesso", () => {
+      const response = {};
+      const request = {};
+      
+      before(() => {
+        request.params = {id: 1}
+        request.body = [{ product_id: 1, quantity: 10}]
+        response.status = sinon.stub().returns(response);
+        response.json = sinon.stub().returns();
 
+        const execute = [{ affectedRows: 1 }];
+        sinon.stub(connection, "execute").resolves(execute);
+
+        sinon.stub(SalesModels, 'updateSale').resolves(true);
+      });
+
+      after(() => {
+        connection.execute.restore();
+        SalesModels.updateSale.restore();
+      });
+
+      it("É chamado um status com código 200", async () => {
+        await SalesControllers.updateSale(request, response);
+        expect(response.status.calledWith(200)).to.be.equal(true);
+      });
+    
     });
   });
 });
